2ba71f59bf94a7935404f071a498ba752615259a
ClemsRex <clementchukwu351@gmail.com>
2025-12-01 22:18:54 +0100

    Completed Navbar and added About

---
+++ src/components/About.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const About = () => {
+  return (
+    <div className='flex flex-col items-center justify-center contaner mx-auto p-14 md:px-20 lg:px-32 w-full overflow-hidden' id='About'>
+
+        <h1 className='text-2xl sm:text-4xl font-bold mb-2 '>About <span className='underline underline-offset-4 decoration-1 under font-light'>Our Company</span></h1>
+
+
+        <p className='text-gray-500 max-w-80 text-center mb-8 '>Here at Arpad Estates, We make a House feel like Home </p>
+
+        <div className='flex flex-col md:flex-row items-center md:items-center md:gap-20'>
+
+            <img src={assets.brand_img} alt="" className='w-full sm:w-1/2 max-w-lg' />
+
+            <div className=' flex flex-col items-center md:items-start mt-10 text-gray-600'>
+
+                <div className=' grid grid-cols-2 gap-6 md:gap-10 w-full 2xl:pr-28 '>
+
+                    <div>
+                        <p className=' text-4xl font-medium text-gray-800'>7+</p>
+                        <p>Years of Excellence</p>
+                    </div>
+                    <div>
+                        <p className=' text-4xl font-medium text-gray-800'>13+</p>
+                        <p>Projects Completed</p>
+                    </div>
+                    <div>
+                        <p className=' text-4xl font-medium text-gray-800'>20+</p>
+                        <p>Mn. Sq. Ft. Delivered</p>
+                    </div>
+                    <div>
+                        <p className=' text-4xl font-medium text-gray-800'>25+</p>
+                        <p>Ongoing Projects</p>
+                    </div>
+
+                </div>
+                <p className='my-10 max-w-lg '> At Arpad, we understand that buying and sellind a property is more than just a transaction, it's a life-changing experience. Our team of expert agents is dedicated to providing personalized service, in depth market Knowledge and a deep understanding of your unique needs. </p>
+                <p className='mb-4'>Let us help you find your dream home or sell your current property with ease and confidence.</p>
+
+                <button className='bg-gray-400 px-8 hover:bg-gray-600 py-2 text-white rounded'> Learn more </button>
+            </div>
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default About
